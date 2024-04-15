c8e6e22ea82e980e74b0904057568f5924004862
Kamal <kamalacca@gmail.com>
2024-04-15 18:47:56 +0400

    dislplay logo

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,20 @@
 import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
-
+import { Image, ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../constants";
 export default function App() {
   return (
-    <View className="bg-gray-100 h-full text-red-300 flex justify-center items-center">
-      <Text className="text-3xl font-pblack">vShare</Text>
-      <Link href="/home" style={{ color: "blue" }}>
-        {" "}
-        Home{" "}
-      </Link>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView
+        contentContainerStyle={{
+          height: "100%",
+        }}
+      >
+        <View className="w-full he-full justify-center items-center p-4">
+          <Image source={images.logo} />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
